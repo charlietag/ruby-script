ebdb6b8892f0f56f8003f48eea838a0705a6eac0
Charlie Hsieh <charlieofficial@gmail.com>
2015-11-23 01:26:46 +0800

    1123 first tried optparse, openstruct

--- script_opt_pp.rb
+++ script_opt_pp.rb
@@ -1,4 +1,30 @@
 #!/usr/bin/env ruby
 
 #put test script here
+require 'optparse'
+require 'ostruct'
+require 'pp'
 
+ostruct = OpenStruct
+data = ostruct.new
+#data = Hash.new
+
+opt_parser = OptionParser.new do |opts|
+  opts.on("-n","--name [name]",String,"Put your name here") do |n|
+    #data[:name] = n
+    data.name = n
+  end
+
+  opts.on("-a","--age [age]", Integer,"What's your age") do |a|
+    #data[:age] = a
+    data.age = a
+  end
+
+  opts.on("-e","--eight [height]",Integer,"What's your height") do |e|
+    #data[:height] = e
+    data.height = e
+  end
+end
+opt_parser.parse!
+puts "My name is #{data.name}. I'm #{data.age} years old. I'm in #{data.height} height."
+pp data
