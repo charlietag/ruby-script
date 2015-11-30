bf736002e17811367e3a0041323a97fef3ee88d2
Charlie Hsieh <charlieofficial@gmail.com>
2015-11-30 22:31:59 +0800

    try all os command via ruby

--- script_os_command.rb
+++ script_os_command.rb
@@ -1,4 +1,56 @@
 #!/usr/bin/env ruby
 
-#put test script here
+def sep_line
+  puts "-------------"
+  puts "    #{Time.now}"
+  puts "-------------"
+end
 
+
+#put test script here
+test_ls = %Q{sleep 10;ls}
+
+#----------Try Iterator-----------
+#call command can just return a string. I have to convert it into array to use iterater (Not IFS like bash)
+#Better way here is use Dir built-in Class
+result_x_each = %x(#{test_ls})
+result_x_each = result_x_each.split(' ')
+result_x_each.each do |x|
+  puts "File : #{x}"
+end
+sep_line
+
+#----------%x-------------
+result_x = %x(#{test_ls})
+puts $? #This identies fork process ID
+puts $?.pid
+puts $?.exitstatus
+puts $$ #This identies main process ID (ruby itself)
+# all above is belong to a bash pid (sid-session id), which is your console  or your screen tab, Do not stop program by killing this,
+# Kill $$ if need
+#puts $0
+sep_line
+
+#=================The following fork process==============
+#----------exec ----------
+#Notice: this will "exit" after execute the command, not really useful
+#result_exec = exec(test_ls)
+##Note: this following will never be reached beacuse of the line above
+#puts $?
+#puts $?.pid
+#puts $?.exitstatus
+#puts $$
+#sep_line
+
+#----------system-------------
+#Notice: Just like %w or backticks, But! This will not use stdout, cannot assign it to a var
+#But will return true or false to determine if the result is OK
+#Not useful as well.
+#The result can be replace by exitstatus via %x
+result = system(test_ls)
+puts result
+puts $?
+puts $?.pid
+puts $?.exitstatus
+puts $$
+sep_line
