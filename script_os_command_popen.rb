c4565d0b296de4b6f9c86f73f158b32ff3c6f307
Charlie Hsieh <charlieofficial@gmail.com>
2015-12-01 01:08:49 +0800

    try io popen

---
+++ script_os_command_popen.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+#put test script here
+#require 'open3'
+#Open3.popen3("ls x") do |i,o,e,t| 
+#  #puts "--stdout--"
+#  #p o.readline
+#  #puts "--stderr--"
+#  p o.readline + e.readline
+#end
+
+#f = open('|ls x')
+##p f.readlines
