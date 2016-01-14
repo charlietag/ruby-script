e52d2c136d3c9e61d451b998a390d8e8febfc32e
Charlie Hsieh <charlieofficial@gmail.com>
2016-01-14 21:39:00 +0800

    try mechanize

---
+++ ruby_mechanize.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+
+require 'mechanize'
+
+#mechanize = Mechanize.new
+#
+#page = mechanize.get('http://www.google.com/')
+#
+#google_form = page.form('f')
+#google_form["q"] = 'ruby mechanize'
+#page = mechanize.submit(google_form, google_form.buttons.first)
+#p page
