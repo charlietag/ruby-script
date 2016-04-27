ac8c780afe4e062fc7c1dd1f430d1957cb403817
Charlie Hsieh <charlieofficial@gmail.com>
2016-04-27 22:01:24 +0800

    change var content

--- ruby_mechanize.rb
+++ ruby_mechanize.rb
@@ -2,11 +2,19 @@
 
 require 'mechanize'
 
-#mechanize = Mechanize.new
-#
-#page = mechanize.get('http://www.google.com/')
-#
+agent = Mechanize.new
+
+#page = agent.get('http://www.google.com/')
+page = agent.get('https://laravel.com/')
+
 #google_form = page.form('f')
 #google_form["q"] = 'ruby mechanize'
 #page = mechanize.submit(google_form, google_form.buttons.first)
 #p page
+
+#page.links.each do |link|
+#    puts link.text
+#end
+
+ads = page.link_with(:text => 'Documentation').click
+pp ads
