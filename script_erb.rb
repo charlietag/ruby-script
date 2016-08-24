0c70f904352c6f62f7a6394ba1af9d3d21ba3415
Charlie Hsieh <charlieofficial@gmail.com>
2016-08-24 08:14:15 +0800

    try ruby erb usage

---
+++ script_erb.rb
@@ -0,0 +1,40 @@
+#!/usr/local/bin/ruby
+require "erb"
+
+class Render
+  #attr_accessor :name, :file
+  def initialize(name = "", file = __FILE__)
+    @name = name
+    @file = file
+  end
+
+  def render
+    template = IO.read @file
+    render = ERB.new template
+    render.result
+  end
+end
+template_file = "script_erb.template"
+
+name = "Charlie"
+output = Render.new name,template_file
+puts output.render
+
+name = "haha"
+output1 = Render.new name,template_file
+puts output1.render
+puts output.render
+#----------------------------------------
+# Using without class
+#----------------------------------------
+
+
+template_file = "script_erb.template"
+template = IO.read template_file
+render3 = ERB.new template
+
+name = "output3"
+output3 = render3.result
+puts output1.render
+puts output.render
+puts output3
